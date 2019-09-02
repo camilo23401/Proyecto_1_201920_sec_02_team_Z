2dc335204db6854ca16e4bdbbd09449ed4fab582
camilo23401 <ca.garciav@uniandes.edu.co>
2019-09-02 17:17:39 -0500

    Se completó la clase ViajeUber

--- proyecto_1/src/mundo/ViajeUber.java
+++ proyecto_1/src/mundo/ViajeUber.java
@@ -5,16 +5,26 @@ public class ViajeUber
 
 	private String sourceid;
 	private String dstid;
+	private String mes;
+	private String dia;
 	private String hora;
 	private String tiempoPromedio;
+	private String desviacionEstandarTiempo;
+	private String tiempoPromedioGeometrico;
+	private String desviacionEstandarTiempoGeometrico;
 
 
-	public ViajeUber(String pSourceid, String pDstid, String pHora, String pTiempoPromedio)
+	public ViajeUber(String pSourceid, String pDstid, String pHora, String pTiempoPromedio, String pMes, String pDia, String pDesviacionEstandarTiempo, String pTiempoPromedioGeometrico, String pDesviacionEstandarTiempoGeometrico)
 	{
 		sourceid = pSourceid;
 		dstid = pDstid;
+		mes = pMes;
+		dia = pDia;
 		hora = pHora;
 		tiempoPromedio = pTiempoPromedio;
+		desviacionEstandarTiempo = pDesviacionEstandarTiempo;
+		tiempoPromedioGeometrico = pTiempoPromedioGeometrico;
+		desviacionEstandarTiempoGeometrico = pDesviacionEstandarTiempoGeometrico;
 	}
 	public String darSourceid()
 	{
@@ -24,6 +34,14 @@ public class ViajeUber
 	{
 		return dstid;
 	}
+	public String darMes()
+	{
+		return mes;	
+	}
+	public String darDia()
+	{
+		return dia;
+	}
 	public String darHora()
 	{
 		return hora;
@@ -31,6 +49,18 @@ public class ViajeUber
 	public String darTiempoPromedio()
 	{
 		return tiempoPromedio;
+	}
+	public String darDesviacionEstandarTiempo()
+	{
+		return desviacionEstandarTiempo;
+	}
+	public String darTiempoPromedioGeometrico()
+	{
+		return tiempoPromedioGeometrico;
+	}
+	public String darDesviacionEstandarTiempoGeometrico()
+	{
+		return desviacionEstandarTiempoGeometrico;
 	}
 }
 
